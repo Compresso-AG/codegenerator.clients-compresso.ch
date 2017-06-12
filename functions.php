74b856fba0781ad7b49d018ebd2a6e0002c3d675
Samuel Rüegger <samuel.ruegger@compresso.ch>
2017-06-12 21:44:47 +0200

    perfmance improvments

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 function generateRandomString($length = 10, $characters) {
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
+	$charactersLength = strlen($characters);
+	$randomString = '';
+	for ($i = 0; $i < $length; $i++) {
+		$randomString .= $characters[rand(0, $charactersLength - 1)];
+	}
+	return $randomString;
 }
 ?>
