ac8d7ff840eb443247f429c89f4d39f277160dc5
Samuel Rüegger <samuel@rueegger.me>
2021-04-14 15:37:09 +0200

    some changes

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if(@isset($_POST['start'])):
 	for($i=0; $i < $_POST['forcodes']; $i++){
 		$randomstring = generateRandomString($_POST['anzzeichen'],$_POST['zeichen']);
 		if(!in_array($randomstring, $keys)){
-			$qr_link = 'https://action-world.win/wincode.php?code='.$randomstring;
+			$qr_link = 'https://beerbingo.ch/c.php?c='.$randomstring;
 			array_push($keys, $randomstring);
 			fwrite($myfile, $randomstring.','.$qr_link."\n");
 			fputcsv($csvfile, array($randomstring, $qr_link));
